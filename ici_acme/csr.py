dcffe1fcdd7cf0e95d36af0f7e1082db39e80af5
Fredrik Thulin <fredrik@thulin.net>
2019-03-28 18:58:15 +0100

    verify that subjectAltNames match the order identifiers too

--- ici_acme/csr.py
+++ ici_acme/csr.py
@@ -1,5 +1,8 @@
+from typing import List, Mapping
+
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.x509.oid import ExtensionOID
 
 from ici_acme.context import Context
 from ici_acme.data import Order
@@ -16,18 +19,41 @@ def validate(data: str, order: Order, context: Context) -> bool:
         context.logger.error(f'Order {order.id} CSR has invalid signature')
         return False
 
-    subj_str = '/'.join([x.rfc4514_string() for x in csr.subject])
-    context.logger.info(f'Order {order.id} subject: {subj_str}')
-    # Check that the subject is exactly a CN that is found in the orders identifiers
-    subj_valid = False
-    for ident in order.identifiers:
-        if ident['type'] == 'dns' and subj_str == f'CN={ident["value"]}':
-            subj_valid = True
-            break
-    if not subj_valid:
-        context.logger.error(f'Order {order.id} subject {subj_str} does not match '
+    subj_list = [x.rfc4514_string() for x in csr.subject]
+    subj_str = '/'.join(subj_list)
+    context.logger.info(f'Order {order.id} subject: "{subj_str}"')
+
+    # Check the commonName
+    cn = [x for x in subj_list if x.startswith('CN=')]
+    if not cn:
+        context.logger.error(f'Order {order.id} subject {subj_str} does not have a commonName')
+        return False
+    cn = cn[0][3:]
+
+    if not _matches_identifiers(cn, order):
+        context.logger.error(f'Order {order.id} commonName "{cn}"" does not match '
                              f'order identifiers {order.identifiers}')
         return False
 
+    # Check all subjectAltNames and reject any other certificate extensions
+    for ext in csr.extensions:
+        if ext.oid == ExtensionOID.SUBJECT_ALTERNATIVE_NAME:
+            for san in ext.value:
+                context.logger.info(f'Order {order.id} subjectAltName: "{san.value}"')
+                if not _matches_identifiers(san.value, order):
+                    context.logger.error(f'Order {order.id} subjectAltName "{san.value}"" does not match '
+                                         f'order identifiers {order.identifiers}')
+                    return False
+        else:
+            context.logger.error(f'Order {order.id} CSR has unknown extensions {ext.oid}')
+            return False
+
     context.logger.info(f'Order {order.id} CSR validated OK')
     return True
+
+
+def _matches_identifiers(name: str, order: Order) -> bool:
+    for ident in order.identifiers:
+        if ident['type'] == 'dns' and ident['value'] == name:
+            return True
+    return False
