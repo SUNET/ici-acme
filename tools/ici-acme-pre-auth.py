55fca8c2c7df03c5c16d3f8dbd29261d998e2137
Johan Lundberg <lundberg@sunet.se>
2019-04-09 17:40:09 +0200

    Restructure ici-acme-pre-auth

--- tools/ici-acme-pre-auth.py
+++ tools/ici-acme-pre-auth.py
@@ -5,6 +5,7 @@ import sys
 import json
 import argparse
 import requests
+import datetime
 
 import logging
 import logging.handlers
@@ -12,75 +13,89 @@ import logging.handlers
 from base64 import b64encode
 from typing import Mapping
 
+from OpenSSL import crypto as openssl_crypto
 import jose
 import jose.constants
 from jose import jwt, jwk, jws
 
 
-logger = None
+logger = logging.getLogger()
 
-_defaults = {'syslog': False,
-             'debug': False,
-             'module': '/path/to/p11-module',
-             'pubkey_id': 'XXX',
-             'cert': '/path/to/cert',
-             'url': 'http://localhost:8000/ici-acme-preauth',
-             }
+_defaults = {
+    'syslog': False,
+    'debug': False,
+    'mode': 'renew',
+    'url': 'http://localhost:8000/ici-acme-preauth',
+}
+
+# _ALG = 'P11TOKEN'
+_ALG = 'RS256'
 
 
 def parse_args(defaults: Mapping):
-    parser = argparse.ArgumentParser(description = 'ICI ACME pre-auth client',
-                                     add_help = True,
-                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter,
-    )
+    parser = argparse.ArgumentParser(description='ICI ACME pre-auth client',
+                                     add_help=True,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     )
 
     # Positional arguments
     # Optional arguments
     parser.add_argument('--dehydrated_account_dir',
-                        dest = 'dehydrated_account_dir',
-                        type = str, required=True,
-                        help = 'Account directory',
-    )
+                        dest='dehydrated_account_dir',
+                        type=str, required=True,
+                        help='Account directory',
+                        )
+    parser.add_argument('--mode',
+                        dest='mode',
+                        type=str, default=defaults['mode'],
+                        help='Pre auth mode',
+                        )
+    parser.add_argument('--cert',
+                        dest='existing_cert',
+                        type=str, required=False,
+                        help='Path to existing certificate',
+                        )
+    parser.add_argument('--key',
+                        dest='private_key',
+                        type=str, required=False,
+                        help='Path to existing private key',
+                        )
     parser.add_argument('--debug',
-                        dest = 'debug',
-                        action = 'store_true', default = defaults['debug'],
-                        help = 'Enable debug operation',
-    )
+                        dest='debug',
+                        action='store_true', default=defaults['debug'],
+                        help='Enable debug operation',
+                        )
     parser.add_argument('--syslog',
-                        dest = 'syslog',
-                        action = 'store_true', default = defaults['syslog'],
-                        help = 'Enable syslog output',
-    )
-    parser.add_argument('--module',
-                        dest = 'module',
-                        type = str, default = defaults['module'],
-                        help = 'PKCS#11 module',
-    )
+                        dest='syslog',
+                        action='store_true', default=defaults['syslog'],
+                        help='Enable syslog output',
+                        )
     parser.add_argument('--url',
-                        dest = 'url',
-                        type = str, default = defaults['url'],
-                        help = 'API URL',
-    )
+                        dest='url',
+                        type=str, default=defaults['url'],
+                        help='API URL',
+                        )
     args = parser.parse_args()
     return args
 
 
-def dehydrated_account_sign(data: dict, args: argparse.Namespace, logger: logging.Logger):
-    with open(os.path.join(args.dehydrated_account_dir, 'account_key.pem'), 'r') as fd:
-        key = jwk.construct(fd.read(), jose.constants.ALGORITHMS.RS256)
-
-    with open(os.path.join(args.dehydrated_account_dir, 'registration_info.json'), 'r') as fd:
-        reg_info = json.loads(fd.read())
-
-    headers = {'kid': str(reg_info['id']),
-               }
-    token = jwt.encode(data, key.to_dict(), headers=headers, algorithm=jose.constants.ALGORITHMS.RS256)
-    return token
-
-
-#_ALG = 'P11TOKEN'
-_ALG = 'RS256'
-
+def _config_logger(args: argparse.Namespace, progname: str):
+    # This is the root log level
+    level = logging.INFO
+    if args.debug:
+        level = logging.DEBUG
+    logging.basicConfig(level=level, stream=sys.stderr,
+                        format='%(asctime)s: %(name)s: %(levelname)s %(message)s')
+    logger.name = progname
+    # If stderr is not a TTY, change the log level of the StreamHandler (stream = sys.stderr above) to WARNING
+    if not sys.stderr.isatty() and not args.debug:
+        for this_h in logging.getLogger('').handlers:
+            this_h.setLevel(logging.WARNING)
+    if args.syslog:
+        syslog_h = logging.handlers.SysLogHandler()
+        formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
+        syslog_h.setFormatter(formatter)
+        logger.addHandler(syslog_h)
 
 
 def iciclient_sign(data, key=None, headers=None):
@@ -94,38 +109,58 @@ def iciclient_sign(data, key=None, headers=None):
         jwk.register_key(_ALG, orig_key)
     return res
 
+
 def iciclient_verify(token, pem):
-    #key = jwk.construct(pem, jwk.ALGORITHMS.RS256)
+    # key = jwk.construct(pem, jwk.ALGORITHMS.RS256)
     key = pem
     return jws.verify(token, key, algorithms=[jwk.ALGORITHMS.RS256])
 
 
+def load_private_key(path: str):
+    try:
+        with open(path, 'r') as fd:
+            return jwk.construct(fd.read(), jose.constants.ALGORITHMS.RS256)
+    except TypeError as e:
+        logger.error(f'Could not load key from {path}')
+        logger.error(e)
+        sys.exit(1)
 
 
+def load_pem(path: str):
+    try:
+        with open(path, 'rb') as fd:
+            return openssl_crypto.load_certificate(openssl_crypto.FILETYPE_PEM, fd.read())
+    except TypeError as e:
+        logger.error(f'Could not load certificate from {path}')
+        logger.error(e)
+        sys.exit(1)
 
-def main(args: argparse.Namespace, logger: logging.Logger):
-    #with open(args.certfile, 'rb') as fd:
-    #    pem = fd.read()
 
-    #signed = iciclient.sign({'pubkey': pem.decode('utf-8')}, args.pubkey_id)
-    # Pass x5c" (X.509 Certificate Chain) Header Parameter
+def dehydrated_account_sign(data: str, dehydrated_account_dir: str, logger: logging.Logger):
+    key = load_private_key(os.path.join(dehydrated_account_dir, 'account_key.pem'))
+    with open(os.path.join(dehydrated_account_dir, 'registration_info.json'), 'r') as fd:
+        reg_info = json.loads(fd.read())
+    headers = {'kid': str(reg_info['id'])}
+    token = jws.sign(data.encode('utf-8'), key.to_dict(), headers=headers, algorithm=jose.constants.ALGORITHMS.RS256)
+    return token
 
-    #certificate = crypto.load_certificate(crypto.FILETYPE_PEM, pem)
-    #x5c = b64encode(crypto.dump_certificate(crypto.FILETYPE_ASN1, certificate)).decode('utf-8')
-    #signed = iciclient.sign({'csr': args.csr}, args.pubkey_id, headers=headers)
-    #logger.debug('JWS: {}'.format(signed))
 
-    identifers = [{'type': 'dns',
-                   'value': 'test.test',
-                   }]
-    #data = {'v': 1,
-    #        'x5c': x5c,
-    #        'signature': identifiers_sig,
-    #        'data': identifers,
-    #        }
-    data = {'identifers': identifers}
+def create_renew_pre_auth(existing_certificate_path: str, existing_key_path: str):
 
-    signed = dehydrated_account_sign(data, args, logger)
+    existing_certificate = load_pem(existing_certificate_path)
+    certificate = b64encode(openssl_crypto.dump_certificate(
+        openssl_crypto.FILETYPE_ASN1, existing_certificate)).decode('utf-8')
+
+    headers = {'x5c': [certificate]}
+    data = {'exp': str(datetime.datetime.utcnow() + datetime.timedelta(seconds=300))}
+
+    key = load_private_key(existing_key_path)
+    return jwt.encode(data, key.to_dict(), headers=headers, algorithm=jose.constants.ALGORITHMS.RS256)
+
+
+def post_pre_auth(dehydrated_path: str, url: str, data: str):
+
+    signed = dehydrated_account_sign(data, dehydrated_path, logger)
     logger.debug(f'Signed data: {signed}')
 
     _elem = signed.split('.')
@@ -133,41 +168,22 @@ def main(args: argparse.Namespace, logger: logging.Logger):
                 'payload': _elem[1],
                 'signature': _elem[2],
                 }
-    if args.url:
-        r = requests.post(args.url, json=req_data)
-        logger.debug('Response from server: {}\n{}\n'.format(r, r.text))
 
+    r = requests.post(url, json=req_data)
+    logger.debug('Response from server: {}\n{}\n'.format(r, r.text))
     return True
 
 
-def _get_logger(args: argparse.Namespace, progname: str) -> logging.Logger:
-    # This is the root log level
-    level = logging.INFO
-    if args.debug:
-        level = logging.DEBUG
-    logging.basicConfig(level=level, stream=sys.stderr,
-                        format='%(asctime)s: %(name)s: %(levelname)s %(message)s')
-    logger = logging.getLogger(progname)
-    # If stderr is not a TTY, change the log level of the StreamHandler (stream = sys.stderr above) to WARNING
-    if not sys.stderr.isatty() and not args.debug:
-        for this_h in logging.getLogger('').handlers:
-            this_h.setLevel(logging.WARNING)
-    if args.syslog:
-        syslog_h = logging.handlers.SysLogHandler()
-        formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
-        syslog_h.setFormatter(formatter)
-        logger.addHandler(syslog_h)
-    return logger
-
-
-if __name__ == '__main__':
+def main():
     try:
         # initialize various components
         progname = os.path.basename(sys.argv[0])
         args = parse_args(_defaults)
-        logger = _get_logger(args, progname)
-
-        res = main(args, logger)
+        _config_logger(args, progname)
+        res = False
+        if args.mode == 'renew':
+            pre_auth = create_renew_pre_auth(args.existing_cert, args.private_key)
+            res = post_pre_auth(args.dehydrated_account_dir, args.url, pre_auth)
 
         if res is True:
             sys.exit(0)
@@ -176,3 +192,7 @@ if __name__ == '__main__':
         sys.exit(int(res))
     except KeyboardInterrupt:
         sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
